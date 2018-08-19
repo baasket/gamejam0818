90ca1d9ee0f11994954812cfd64959c8b1a4c0b5
Fabien Bourquin <fabien2074@hotmail.com>
2018-08-19 14:58:09 +0200

    Ajouter menuCrédit et HowTo

--- Game Jam 18/Assets/Scripts/MainMenuManager.cs
+++ Game Jam 18/Assets/Scripts/MainMenuManager.cs
@@ -1,13 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MainMenuManager : MonoBehaviour
 {
+    
+    public GameObject mainCanvas;
+    public GameObject howToCanvas;
+    public GameObject creditCanvas;
+
     public void loadGameScene()
     {
         SceneManager.LoadScene("gameScene");
     }
 
+    public void displayCredit(){
+        mainCanvas.active=false;
+        howToCanvas.active=false;
+        creditCanvas.active=true;
+    }
+
+    public void displayHowTo(){
+        mainCanvas.active=false;
+        creditCanvas.active=false;
+        howToCanvas.active=true;
+    }
+
+    public void displayMainCanvas(){
+        creditCanvas.active=false;
+        howToCanvas.active=false;
+        mainCanvas.active=true;
+    }
 }
